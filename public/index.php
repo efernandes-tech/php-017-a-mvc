ab4252ce845ccd2d1b94564f906a90a3d9aaad68
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-09-06 12:31:13 -0300

    08 Usando Rotas

--- public/index.php
+++ public/index.php
@@ -2,24 +2,20 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Alura\Cursos\Controller\ListarCursos;
-use Alura\Cursos\Controller\FormularioInsercao;
-use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
-switch ($_SERVER['PATH_INFO']) {
-    case '/listar-cursos':
-        $controlador = new ListarCursos();
-        $controlador->processaRequisicao();
-        break;
-    case '/novo-curso':
-        $controlador = new FormularioInsercao();
-        $controlador->processaRequisicao();
-        break;
-    case '/salvar-curso':
-        $controlador = new Persistencia();
-        $controlador->processaRequisicao();
-        break;
-    default:
-        echo "Erro 404";
-        break;
+$caminho = $_SERVER['PATH_INFO'];
+$rotas = require __DIR__ . '/../config/routes.php';
+
+if ( ! array_key_exists($caminho, $rotas)) {
+    http_response_code(404);
+    exit();
 }
+
+$classControladora = $rotas[$caminho];
+
+/**
+ * @var InterfaceControladorRequisicao $controlador
+ */
+$controlador = new $classControladora();
+$controlador->processaRequisicao();
